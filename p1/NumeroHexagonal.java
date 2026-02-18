c04201b2eca4b1b3309530a2d55024bdc4a74500
HectorEsp78 <hector.espinosa@alu.uclm.es>
2026-02-18 15:42:07 +0100

    Completados métodos iterativos

--- p1/NumeroHexagonal.java
+++ p1/NumeroHexagonal.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 public class NumeroHexagonal {
+    public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        System.out.println("Calculo del numero hexagonal N.");
+        System.out.print("Introduce el valor de N: ");
+        int n = sc.nextInt();
+
+        System.out.printf("Calculo con formula: %d\n", calculoFormula(n));
+        System.out.printf("Calculo con serie iterativa: %d\n", calculoSerieIterativo(n));
+        System.out.printf("Calculo con serie recursiva: %d\n", calculoSerieRecursivo(n));
     }
 
-    public static int calculoFormula(){
+    /*
+    La complejidad asintotica es del orden O(n²), ya que el tamaño del problema se inluye en la fórmula, con n²
+    */
+    public static int calculoFormula(int n){
+        return (2*n*n)-n;
+    }
+
+    /*
+    La complejidad asintotica es del orden O(n), ya que la parte más costosa del método (bucle for)
+    se ejecuta n veces, el resto son constantes que no aumentan con el tamaño del problema.
+    */
+    public static int calculoSerieIterativo(int n){
+        int h = 0; // 1
+        for(int i = 0; i < n; i++){ // 1 + 2n
+            h += 4*i+1; // 4
+        }
+        return h; // 1
+    }
+    public static int calculoSerieRecursivo(int n){
         return 0;
     }
 }
