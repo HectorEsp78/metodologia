8cada0ee06e526081e057fb910bf0d7ad54c68c8
SmokierParty427 <oscarjcastillo186@gmail.com>
2026-02-24 11:57:04 +0100

    Añadida parte 2 de la practica

--- p1/NumeroHexagonal.java
+++ p1/NumeroHexagonal.java
@@ -43,7 +43,7 @@ public class NumeroHexagonal {
     public static int calculoSerieRecursivo(int n) {
         n--; // 1
         int h; // 1
-        if (n == 0)  // 1
+        if (n == 0) // 1
             h = 1; // 1
         else
             h = 4 * n + 1 + calculoSerieRecursivo(n); // 4 + T(n-1)
@@ -51,12 +51,13 @@ public class NumeroHexagonal {
     }
 
     public static void MostrarTabla(boolean nano) {
-        int[] n = { 10, 100, 500, 1000, 5000, 8000 , 10000, 11000, 12000};
+        int[] n = { 10, 100, 500, 1000, 5000, 8000, 10000, 11000, 12000 };
         System.out.println("----------------------------------------------------------------------------");
-        System.out.printf("| %-5s | %-10s | %-15s | %-15s | %-15s |%n", "N", "Valor", "Fórmula", "Iterativo", "Recursivo");
+        System.out.printf("| %-5s | %-10s | %-15s | %-15s | %-15s |%n", "N", "Valor", "Fórmula", "Iterativo",
+                "Recursivo");
         System.out.println("----------------------------------------------------------------------------");
-        
-        for(int i=0; i<n.length; i++){
+
+        for (int i = 0; i < n.length; i++) {
             long inicio = System.nanoTime();
             int valorFormula = calculoFormula(n[i]);
             long fin = System.nanoTime();
@@ -72,19 +73,19 @@ public class NumeroHexagonal {
             fin = System.nanoTime();
             double duracionRecursivo = fin - inicio;
 
-            if(!nano){
+            if (!nano) {
                 duracionFormula /= 1000000; // Convertir a milisegundos
                 duracionIterativo /= 1000000; // Convertir a milisegundos
                 duracionRecursivo /= 1000000; // Convertir a milisegundos
 
-                System.out.printf("| %-5d | %-10d | %-12.6f ms | %-12.6f ms | %-12.6f ms |%n", n[i], valorFormula, duracionFormula, duracionIterativo, duracionRecursivo);
-            } else{
-                System.out.printf("| %-5d | %-10d | %-12.2f ns | %-12.2f ns | %-12.2f ns |%n", n[i], valorFormula, duracionFormula, duracionIterativo, duracionRecursivo);
+                System.out.printf("| %-5d | %-10d | %-12.6f ms | %-12.6f ms | %-12.6f ms |%n", n[i], valorFormula,
+                        duracionFormula, duracionIterativo, duracionRecursivo);
+            } else {
+                System.out.printf("| %-5d | %-10d | %-12.2f ns | %-12.2f ns | %-12.2f ns |%n", n[i], valorFormula,
+                        duracionFormula, duracionIterativo, duracionRecursivo);
             }
 
-            
         }
-    
-    
+
     }
 }
