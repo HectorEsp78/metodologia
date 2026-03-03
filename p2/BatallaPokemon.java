6faf1d76bf295f0e959f8cc4c50c5a1115070331
HectorEsp78 <hector.espinosa@alu.uclm.es>
2026-03-03 12:39:11 +0100

    Lectura fichero

--- p2/BatallaPokemon.java
+++ p2/BatallaPokemon.java
@@ -1,32 +1,34 @@
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class BatallaPokemon{
-    
+class BatallaPokemon {
+
+    public static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         // es importante usar la ruta correcta relativa al directorio desde el
         // que se ejecuta el programa; en nuestro caso el fichero está en p2/
-        int[] danos = leerArchivoDanos("p2/DatosPokemon1.txt");
-        System.out.println("Daños leídos: " + java.util.Arrays.toString(danos));
+        List<Integer> danos = leerArchivoDanos("p2/DatosPokemon1.txt");
+        System.out.println("Daños leídos: " + danos.toString());
     }
 
-    public static int[] leerArchivoDanos(String nombreArchivo) {
+    public static List<Integer> leerArchivoDanos(String nombreArchivo) {
         // el archivo de ejemplo no tiene un tamaño en la primera posición,
         // por eso iteraremos hasta que no queden enteros. Usamos una lista
         // para poder almacenar una cantidad desconocida de valores.
-        try (Scanner scanner = new Scanner(new java.io.File(nombreArchivo))) {
-            java.util.List<Integer> lista = new java.util.ArrayList<>();
-            while (scanner.hasNextInt()) {
-                lista.add(scanner.nextInt());
-            }
-            int[] danos = new int[lista.size()];
-            for (int i = 0; i < lista.size(); i++) {
-                danos[i] = lista.get(i);
-            }
-            return danos;
-        } catch (java.io.FileNotFoundException e) {
-            System.out.println("Archivo no encontrado: " + nombreArchivo);
-            return null;
+        try {
+            Scanner sc = new Scanner(new File(nombreArchivo));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        List<Integer> danos = new ArrayList<>();
+        while (sc.hasNextInt()) {
+            danos.add(sc.nextInt());
+        }
+        return danos;
     }
 }
