92d687e87b3fee5029ee7ccf655b75b1e1c687f4
HectorEsp78 <hector.espinosa@alu.uclm.es>
2026-03-03 18:52:18 +0100

    Metodo recursivo terminado, metodos aux

--- p2/BatallaPokemon.java
+++ p2/BatallaPokemon.java
@@ -12,57 +12,117 @@ class BatallaPokemon {
         List<Integer> danos = leerArchivoDanos("p2/DatosPokemon1.txt");
         System.out.println("Daños leídos: " + danos.toString());
 
-        System.out.println("Iterativo: " + sumaDanosIterativo(danos, "A"));
-        System.out.println("Recursivo: " + sumaDanosRecursivo(danos, "A", 0, danos.size()-1));
+        System.out.print("Elige un entrenador (A/B): ");
+        String entrenador = sc.next().toUpperCase();
+        System.out.println("\n");
+
+        if (entrenador.equals("A") || entrenador.equals("B")) {
+            probarMetodos(danos, entrenador);
+            int ganador = ganador(danos);
+            switch (ganador) {
+                case 0:
+                    System.out.println("EL GANADOR DE LA BATALLA ES: A");
+                    break;
+                case 1:
+                    System.out.println("EL GANADOR DE LA BATALLA ES: B");
+                    break;
+                default:
+                    System.out.println("EMPATE!");
+                    break;
+            }
+        } else
+            System.out.println("Opcion invalida, finalizando...");
+
     }
 
     public static List<Integer> leerArchivoDanos(String nombreArchivo) {
         List<Integer> danos = new ArrayList<>();
         try {
-            Scanner sc = new Scanner(new File(nombreArchivo));
-            while (sc.hasNextInt()) {
-                danos.add(sc.nextInt());
+            Scanner lector = new Scanner(new File(nombreArchivo));
+            while (lector.hasNextInt()) {
+                danos.add(lector.nextInt());
             }
+            lector.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.printf("Error leyendo el archivo %s\n", nombreArchivo);
+            System.exit(1);
         }
-
         return danos;
     }
 
-    public static int sumaDanosIterativo(List<Integer> danos, String entrenador){
+    public static int sumaDanosIterativo(List<Integer> danos, String entrenador) {
         int suma = 0;
         int i = 0;
-        if(entrenador.toLowerCase().equals("a")){
-            while(i < danos.size()){
-                if(danos.get(i) > 0){
+        if (entrenador.toLowerCase().equals("a")) {
+            while (i < danos.size()) {
+                if (danos.get(i) > 0) {
                     suma += danos.get(i);
                 }
                 i++;
             }
         } else {
-            while(i < danos.size()){
-                if(danos.get(i) < 0){
-                    suma += danos.get(i)*-1;
+            while (i < danos.size()) {
+                if (danos.get(i) < 0) {
+                    suma += danos.get(i) * -1;
                 }
                 i++;
             }
         }
-        
+
         return suma;
     }
 
-    public static int sumaDanosRecursivo(List<Integer> v, String entrenador, int li, int ls){
-        if(ls <= li){
-            if((entrenador.toLowerCase().equals("a") && v.get(0) > 0) || (entrenador.toLowerCase().equals("b") && v.get(0) < 0)){
-                return v.get(0);
+    public static int sumaDanosRecursivo(List<Integer> v, String entrenador, int li, int ls) {
+        if (ls == li) {
+            if ((entrenador.toUpperCase().equals("A") && v.get(li) > 0)
+                    || (entrenador.toUpperCase().equals("B") && v.get(li) < 0)) {
+                return v.get(li);
             } else {
                 return 0;
             }
         }
-        int mitad = (li+ls)/2;
-        return sumaDanosRecursivo(v, entrenador, li, mitad-1) + sumaDanosRecursivo(v, entrenador, mitad, ls);
-        
-        
+        int mitad = (li + ls) / 2;
+        int sumaIzq = sumaDanosRecursivo(v, entrenador, li, mitad);
+        int sumaDer = sumaDanosRecursivo(v, entrenador, mitad + 1, ls);
+        return sumaIzq + sumaDer;
+    }
+
+    public static void probarMetodos(List<Integer> danos, String entrenador) {
+        long tInicio, tFinal;
+        int resIterativo, resRecursivo;
+
+        tInicio = System.nanoTime();
+        resIterativo = sumaDanosIterativo(danos, entrenador);
+        tFinal = System.nanoTime();
+
+        System.out.printf("Daño neto del entrenador %s (Iterativo): %d (Tiempo: %,dns)\n", entrenador, resIterativo,
+                (tFinal - tInicio));
+
+        tInicio = System.nanoTime();
+        resRecursivo = sumaDanosRecursivo(danos, entrenador, 0, danos.size() - 1);
+        tFinal = System.nanoTime();
+
+        if (resRecursivo < 0)
+            resRecursivo *= -1;
+
+        System.out.printf("Daño neto del entrenador %s (Recursivo): %d (Tiempo: %,dns)\n", entrenador, resRecursivo,
+                (tFinal - tInicio));
+
+    }
+
+    public static int ganador(List<Integer> danos) {
+        int ganador;
+
+        int danoA = sumaDanosRecursivo(danos, "A", 0, danos.size() - 1);
+        int danoB = sumaDanosRecursivo(danos, "B", 0, danos.size() - 1) * -1;
+
+        if (danoA > danoB)
+            ganador = 0;
+        else if (danoB < danoA)
+            ganador = 1;
+        else
+            ganador = -1;
+
+        return ganador;
     }
 }
