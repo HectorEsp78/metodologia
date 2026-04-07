446e676522db2daf3f5d364547bb8ec936acff12
HectorEsp78 <hector.espinosa@alu.uclm.es>
2026-04-07 11:39:29 +0200

    p3

--- p3/Local.java
+++ p3/Local.java
@@ -1,92 +1,25 @@
+/**
+ * Representa el local rectangular que se debe enlosar.
+ */
+public class Local {
+    private int ancho;
+    private int largo;
 
-import java.util.Arrays;
-
-
-/*
-
-    1. Funcion objetivo: minimizar num baldosas
-    2. Candidatos: {(1,1),(10,10),(15,15),(50,50)}
-    3. Seleccionados: {}
-    4. Funcion solucion: queda suelo sin baldosas?
-    5. Funcion factibilidad: cabe x baldosa en el espacio disponible?
-    6. Funcion seleccion: elige la baldosa mas grande que quepa
-
-*/
-
-public class Local{
-    public static void main(String[] args) {
-        int[][] candidatos = {{50,50},{15,15},{10,10},{1,1}};
-        int[] dimLocal = {200,201};
-
-        System.out.println(Arrays.toString(nBaldosas(candidatos, dimLocal)));
-
+    public Local(int ancho, int largo) {
+        if (ancho <= 0 || largo <= 0)
+            throw new IllegalArgumentException("Las dimensiones del local deben ser enteras positivas.");
+        this.ancho = ancho;
+        this.largo = largo;
     }
 
-    public static int[] nBaldosas(int[][] candidatos, int[] dimLocal){
-        int[] nBaldosas = new int[candidatos.length];
-        for(int i = 0; i < nBaldosas.length; i++){
-            nBaldosas[i] = 0;
-        }
+    public int getAncho() { return ancho; }
+    public int getLargo() { return largo; }
 
-        boolean[][] cubierto = new boolean[dimLocal[0]][dimLocal[1]];
-        for(int i = 0; i < cubierto.length; i++){
-            for(int j = 0; j < cubierto[0].length; j++){
-                cubierto[i][j] = false;
-            }
-        }
-        
-        while(!solucionado(cubierto)){
-            int i = 0;
-            int[] baldosa = candidatos[i];
+    /** Área total del local */
+    public int area() { return ancho * largo; }
 
-            int[] pos = cabe(baldosa, cubierto);
-            while(pos[0] == -1){
-                i++;
-                baldosa = candidatos[i];
-            }
-            colocar(baldosa, cubierto, pos);
-            nBaldosas[i]++;
-        }
-
-        return nBaldosas;
-    }
-
-    public static int[] cabe(int[] baldosa, boolean[][] cubierto){
-        int filas = cubierto.length;
-        int cols  = cubierto[0].length;
-        int alto  = baldosa[0];
-        int ancho = baldosa[1];
-
-        for (int i = 0; i <= filas - alto; i++) {
-            for (int j = 0; j <= cols - ancho; j++) {
-                // Comprobar si el rectángulo completo está libre
-                boolean encaja = true;
-                for (int di = 0; di < alto && encaja; di++)
-                    for (int dj = 0; dj < ancho && encaja; dj++)
-                        if (cubierto[i + di][j + dj]) encaja = false;
-
-                if (encaja) return new int[]{i, j};
-            }
-        }
-        return new int[]{-1, -1}; // no cabe
-    }
-
-    public static boolean solucionado(boolean[][] cubierto){
-        boolean solucionado = true;
-        for(int i = 0; i < cubierto.length; i++){
-            for(int j = 0; j < cubierto[0].length; j++){
-                if(!cubierto[i][j]) solucionado = false;
-            }
-        }
-        return solucionado;
-    }
-
-    public static void colocar(int[] baldosa, boolean[][] cubierto, int[] pos){
-
-        for(int i = pos[0]; i < pos[0]+baldosa[0]; i++){
-            for(int j = pos[1]; j < pos[1]+baldosa[1]; j++){
-                cubierto[i][j] = true;
-            }
-        }
+    @Override
+    public String toString() {
+        return "Local(" + ancho + "x" + largo + ", área=" + area() + ")";
     }
 }
