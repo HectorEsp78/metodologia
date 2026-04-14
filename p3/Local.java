46a8a4eb335cf70434fd13e8c4e35d20a74ca69c
HectorEsp78 <hector.espinosa@alu.uclm.es>
2026-04-14 11:43:35 +0200

    p3

--- p3/Local.java
+++ p3/Local.java
@@ -1,25 +1,55 @@
-/**
- * Representa el local rectangular que se debe enlosar.
- */
-public class Local {
-    private int ancho;
-    private int largo;
 
-    public Local(int ancho, int largo) {
-        if (ancho <= 0 || largo <= 0)
-            throw new IllegalArgumentException("Las dimensiones del local deben ser enteras positivas.");
-        this.ancho = ancho;
-        this.largo = largo;
+/*
+
+    1. Funcion objetivo: minimizar num baldosas
+    2. Candidatos: {(1,1),(5,5),(2,5)}
+    3. Seleccionados: {}
+    4. Funcion solucion: queda suelo sin baldosas?
+    5. Funcion factibilidad: cabe x baldosa en el espacio disponible?
+    6. Funcion seleccion: elige la baldosa mas grande que quepa
+
+*/
+
+public class Local {
+
+
+    public static void main(String[] args){
+        int[] dimLocal = {100,100};
+        int[][] baldosas = {{1,1}, {2,5}, {5,5}};
+        int areaCubierta = 0;
+
+        boolean[][] local = new boolean[dimLocal[0]][dimLocal[1]];
+        for(int f = 0; f < local.length; f++){
+            for(int c = 0; c < local[0].length; c++){
+                local[f][c] = false;
+            }
+        }
+
+        while(!solucion(dimLocal, areaCubierta)){
+            for(int i = 0; i < baldosas.length; i++){
+
+            }
+        }
+      
     }
 
-    public int getAncho() { return ancho; }
-    public int getLargo() { return largo; }
+    public static boolean solucion(int[] dimLocal, int areaCubierta){
+        return (dimLocal[0]*dimLocal[1]) == areaCubierta;
+    }
 
-    /** Área total del local */
-    public int area() { return ancho * largo; }
+    public static boolean cabe(int[] baldosa, boolean[][] local){
+        boolean cabe = false;
+        int cuentaLargo = 0, cuentaAlto = 0;
 
-    @Override
-    public String toString() {
-        return "Local(" + ancho + "x" + largo + ", área=" + area() + ")";
+        for(int f = 0; f < local.length && cuentaLargo < baldosa[0]; f++){
+            for(int c = 0; c < local.length && cuentaAlto < baldosa[1]; c++){
+                if(local[f][c]) cuentaAlto = 0; 
+                else cuentaAlto++;
+            }
+            if(cuentaAlto != baldosa[1]) cuentaLargo = 0;
+            else cuentaLargo++;
+        }
+
+        return cabe;        
     }
 }
